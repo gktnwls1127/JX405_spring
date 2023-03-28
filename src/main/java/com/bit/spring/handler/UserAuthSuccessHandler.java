8a187e4d24cae731ccfdecaebfec433d20f3d2a1
gktnwls1127 <gktnwls1127@naver.com>
2023-03-28 11:44:20 +0900

    "spring board" logging end

--- src/main/java/com/bit/spring/handler/UserAuthSuccessHandler.java
+++ src/main/java/com/bit/spring/handler/UserAuthSuccessHandler.java
@@ -2,6 +2,8 @@ package com.bit.spring.handler;
 
 import com.bit.spring.model.UserCustomDetails;
 import com.bit.spring.model.UserDTO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
@@ -15,15 +17,21 @@ import java.io.IOException;
 @Service
 public class UserAuthSuccessHandler implements AuthenticationSuccessHandler {
 
+    private static final Logger LOGGER = LogManager.getLogger(UserAuthSuccessHandler.class);
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
         //System.out.println(authentication.getName());
         //System.out.println(authentication.getDetails());
+
         UserDTO userDTO = ((UserCustomDetails)authentication.getPrincipal()).getUserDTO();
         userDTO.setPassword(null);
         // 세션 정보 저장
         HttpSession session = httpServletRequest.getSession();
         session.setAttribute("logIn", userDTO);
+
+        //Log 부분
+        String message = "사용자 " + userDTO.getUsername() + "님이 로그인 하셨습니다.";
+        LOGGER.info(message);
 
         httpServletResponse.sendRedirect("/board/showAll/1");
 
